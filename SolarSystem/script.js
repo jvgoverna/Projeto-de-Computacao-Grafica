c0b11348b58a917d42cf1ec0042ecb1982fb6fe6
jvgoverna <jv11governa2018@gmail.com>
2024-10-28 14:19:23 -0300

    ADD click on some object animation

--- SolarSystem/script.js
+++ SolarSystem/script.js
@@ -13,16 +13,18 @@ const sphere = {
 	Sun : {
 		geometry : new THREE.SphereGeometry( 0.5, 32, 32 ),
 		material : new THREE.MeshBasicMaterial( {color : 0xffff00} ),
-		posX : -4.3,
+		posX : -4,
 		scaleX : 2,
-		scaleY : 2
+		scaleY : 2,
+		name : 'Sun',
 	},
 	Mercury : {
 		geometry : new THREE.SphereGeometry( 0.5, 32, 32 ),
 		material : new THREE.MeshBasicMaterial( {color : 0xff0000 } ),
 		posX : 2,
 		scaleX : 1,
-		scaleY : 1
+		scaleY : 1,
+		name : 'Mercury'
 	}
 }
 
@@ -34,10 +36,11 @@ const createSolarSystem = (sphere) => {
 		solarSystem.position.x = sphere[keys]['posX'];
 		solarSystem.scale.x = sphere[keys]['scaleX'];
 		solarSystem.scale.y = sphere[keys]['scaleY'];
-
+		solarSystem.name = sphere[keys]['name'];
 		scene.add( solarSystem);
 	}
 }
+
 createSolarSystem(sphere);
 camera.position.z = 5;
 
@@ -51,21 +54,61 @@ const calculatePointerMovementMouse = (ev) => {
 	raycaster.setFromCamera( pointer , camera );
 }
 
-window.addEventListener("mousemove" , (ev) => {
-	calculatePointerMovementMouse(ev);
-
-	//ao traçar o raio da camera ao objeto e detectar uma "colisão" intersects retorna uma lista de objetos encontrados
+const clickObject = () => {
 	const intersects = raycaster.intersectObjects( scene.children );
 
+	
+	const container = document.querySelector('.container');
+	
+	for( let keys in sphere ){
+		let name = sphere[keys]['name'];
+		
+		if (intersects.length > 0){
+			const clickedObject = intersects[0].object;
+			if(clickedObject.name === name){
+				console.log(name);
+				// const backButton = document.createElement('button');
+				// backButton.setAttribute("class","backButton");
+				// container.appendChild( backButton );
+				const posX = sphere[keys]['posX'];
+
+				if (clickedObject.name === "Sun"){
+
+					if(camera.position.x > posX) {
+						camera.position.x -= 0.2;
+					}
+					if(camera.position.z > 2){
+						camera.position.z -= 0.2;
+					}
+
+					requestAnimationFrame(clickObject);
+				}
+				
+			}
+		}
+		else{
+			console.log("Nenhum Objeto selecionado");
+		}
+	}
+}
+
+window.addEventListener("mousemove" , (ev) => {
+	calculatePointerMovementMouse(ev);
+	
+	//ao traçar o raio da camera ao objeto e detectar uma "colisão" intersects retorna uma lista de objetos encontrados
+	const intersects = raycaster.intersectObjects( scene.children );
+	
 	if(intersects.length > 0){
 		document.body.style.cursor = 'pointer';
 	}else{
 		document.body.style.cursor = 'default';
 	}
-
+	
 	//console.log(intersects);
 })
 
+
+window.addEventListener('click' , clickObject);
 
 window.addEventListener( "resize", () => {
     camera.aspect = innerWidth/innerHeight;
